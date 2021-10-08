6e6c1ee8549ad9ae5a9f1451f9a9f77d328976cc
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-10-08 19:15:37 +0300

    Added User controller and basic AppConfiguration.

---
+++ PathfinderProject/pathfinder/src/main/java/pathfinder/web/UserController.java
@@ -0,0 +1,29 @@
+package pathfinder.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+//    @PostMapping("/register")
+//    public String registerConfirm(UserRegisterBindingDto userRegisterBindingDto,
+//                                  BindingResult bindingResult){
+//        //TODO
+//
+//        return "redirect:/";
+//    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
